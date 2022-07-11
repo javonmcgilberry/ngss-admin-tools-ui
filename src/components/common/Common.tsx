d877c9e6761ade20e2497ef30e1df250cf9019c6
Laura Pettis <933265@cognizant.com>
2022-07-11 13:07:06 -0500

    AT-40 Create Coupon

--- src/components/common/Common.tsx
+++ src/components/common/Common.tsx
@@ -45,3 +45,18 @@ export const Section = ({ title, children }: { children: React.ReactNode; title:
         </div>
     );
 };
+
+export const FlexBox = () => {
+    return (
+        <div className={styles.flex_container_main}>
+            <div className={styles.flex_container_top}>
+                <div className={styles.flex_item}></div>
+                <div className={styles.flex_item}></div>
+            </div>
+            <div className={styles.flex_container_bottom}>
+                <div className={styles.flex_item}></div>
+                <div className={styles.flex_item}></div>
+            </div>
+        </div>
+    );
+};
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,17 @@
 import styles from './layout.module.scss';
 import Sidebar from '../sidebar/Sidebar';
 import Main from '../main/Main';
-import { PageWrapper } from '../common/Common';
+import { FlexBox, PageWrapper } from '../common/Common';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className={styles.app_container}>
             <Sidebar />
             <Main>
-                <PageWrapper>{children}</PageWrapper>
+                <PageWrapper>
+                    {children}
+                    <FlexBox></FlexBox>
+                </PageWrapper>
             </Main>
         </div>
     );
